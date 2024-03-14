2792779055b6f4a2f25c03678a4be847a8a22eee
Suresh Patil <patilrsuresh@outlook.com>
2024-03-13 22:28:44 -0400

    commit 7

--- server.js
+++ server.js
@@ -4,6 +4,10 @@ const axios = require('axios');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+// Adding defult browers
+app.set('view engine', 'ejs');
+
+
 // Middleware
 app.use(express.static('public'));
 
